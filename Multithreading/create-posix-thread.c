03ccc2ae49e4e45344c2f60bce1dc7eb245fb524
Gerald Aguilar <geraldag@usc.edu>
2021-09-25 01:38:14 -0700

    two thread program to update global variable

---
+++ Multithreading/create-posix-thread.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+//some global varible
+int input = 0;
+
+void *server(void *arg)
+{
+    input = (*((int *)arg)) + input;
+    printf("i has value: %d\n", input);
+
+    return NULL;
+}
+
+void start_servers(void)
+{
+    //thread identifiers
+    pthread_t threadOne, threadTwo;
+    int increment = 1;
+
+    //create 2 threads!
+    pthread_create(
+        &threadOne,
+        0,
+        server,
+        &increment);
+
+    pthread_create(
+        &threadTwo,
+        0,
+        server,
+        &increment);
+
+    //tells the program to allow the thread process to terminate.
+    pthread_join(threadOne, NULL);
+    pthread_join(threadTwo, NULL);
+}
+
+int main(void)
+{
+
+    //run our multithreaded program
+    start_servers();
+
+    return EXIT_SUCCESS;
+}
